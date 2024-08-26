b64bea5dc6a029b352972d02dc2c3a400984511f
Mmy2000 <yahianada005@gmail.com>
2024-08-26 22:22:30 +0300

    finishing the project

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import App from './App.jsx'
 import './index.css'
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
